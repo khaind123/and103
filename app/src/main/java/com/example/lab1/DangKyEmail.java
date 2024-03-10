05836ab50daf56f3e55565060bcd13d82fe0228b
khaind123 <124582322+khaind123@users.noreply.github.com>
2024-03-10 08:17:31 +0700

    lab

--- app/src/main/java/com/example/lab1/DangKyEmail.java
+++ app/src/main/java/com/example/lab1/DangKyEmail.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class DangKyEmail extends AppCompatActivity {
@@ -41,7 +46,17 @@ public class DangKyEmail extends AppCompatActivity {
                 em = edtTDK.getText().toString();
                 pw = edtMKDK.getText().toString();
 
-                
+                auth.createUserWithEmailAndPassword(em, pw).addOnCompleteListener(DangKyEmail.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Dang ky thành công", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Dang ky that bai", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
